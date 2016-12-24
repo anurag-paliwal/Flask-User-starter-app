6a8d763c00f0c1c00c6fc22e7269e510a0791063
Anurag Paliwal <anuragpaliwal@live.in>
2016-12-24 21:51:29 +0530

    Initial release with mysql

--- app/create_app.py
+++ app/create_app.py
@@ -87,16 +87,16 @@ def init_email_error_handler(app):
     import logging
     from logging.handlers import SMTPHandler
 
-    mail_handler = SMTPHandler(
-        mailhost=(host, port),  # Mail host and port
-        fromaddr=from_addr,  # From address
-        toaddrs=to_addr_list,  # To address
-        subject=subject,  # Subject line
-        credentials=(username, password),  # Credentials
-        secure=secure,
-    )
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+    # mail_handler = SMTPHandler(
+    #     mailhost=(host, port),  # Mail host and port
+    #     fromaddr=from_addr,  # From address
+    #     toaddrs=to_addr_list,  # To address
+    #     subject=subject,  # Subject line
+    #     credentials=(username, password),  # Credentials
+    #     secure=secure,
+    # )
+    # mail_handler.setLevel(logging.ERROR)
+    # app.logger.addHandler(mail_handler)
 
     # Log errors using: app.logger.error('Some error message')
 
