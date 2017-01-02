ecd8354433916f8e658dac4c7a5da2556402b41e
Anurag Paliwal <anurag.paliwal@gslab.com>
2017-01-02 22:20:47 +0530

    updated test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,11 @@
 cd /application
 # Download requirements to build cache
 pip download -d /build -r requirements.txt --no-input
+pip download -d /build -r requirements_test.txt --no-input
 
 # Install application test requirements
 pip install --no-index -f /build -r requirements.txt
+pip install --no-index -f /build -r requirements_test.txt
 
 # Run test.sh arguments
 exec $@
